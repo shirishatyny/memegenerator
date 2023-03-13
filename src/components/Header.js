c9fbd686583e393ad238fc0179531734065cf9dc
shirishatyny <shirisha.kolukula@tynybay.io>
2023-03-13 06:45:12 +0530

    changes

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+import React from "react"
+
+ function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://freepngimg.com/download/internet_meme/3-2-troll-face-meme-png.png"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
+export default Header
